d811de1648a432d73c5356f287ca12bdd1d816e5
minhnnhe160942 <minhnnhe160942@fpt.edu.vn>
2023-09-14 01:19:42 +0700

    first commit

---
+++ src/main/java/team2/elearningapplication/ElearningSwp391Application.java
@@ -0,0 +1,13 @@
+package team2.elearningapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningSwp391Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElearningSwp391Application.class, args);
+    }
+
+}
