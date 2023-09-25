5a1cf5635afd2f7d11259995cdb1178ab0be78eb
minhnhhe161877 <minhnhhe161877@fpt.edu.vn>
2023-09-25 15:17:29 +0700

    Create Quiz request and response

---
+++ src/main/java/team2/elearningapplication/dto/request/CreateQuizRequest.java
@@ -0,0 +1,25 @@
+package team2.elearningapplication.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import team2.elearningapplication.entity.Lesson;
+import team2.elearningapplication.entity.Question;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreateQuizRequest {
+    @NotNull
+    private Lesson lesson;
+    @NotBlank
+    private String name;
+    @NotEmpty
+    private List<Question> questionList;
+}
