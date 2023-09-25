ab7686dcace21ebc00bf8f460341a26bbf43ad5f
minhnhhe161877 <minhnhhe161877@fpt.edu.vn>
2023-09-25 15:33:28 +0700

    Delete Quiz

--- src/main/java/team2/elearningapplication/dto/request/DeleteQuizRequest.java
+++ src/main/java/team2/elearningapplication/dto/request/DeleteQuizRequest.java
@@ -1,2 +1,26 @@
-package team2.elearningapplication.dto.request;public class DeleteQuizRequest {
+package team2.elearningapplication.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import team2.elearningapplication.entity.Lesson;
+import team2.elearningapplication.entity.Question;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DeleteQuizRequest {
+    @NotNull
+    private Lesson lesson;
+    @NotBlank
+    private String name;
+    @NotEmpty
+    private List<Question> questionList;
 }
