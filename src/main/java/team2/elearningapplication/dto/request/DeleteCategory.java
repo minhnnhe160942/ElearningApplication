898334f5a3f3793433dcd432f1a064ec9894756e
quangcvvhe160953 <quangcvvhe160953@fpt.edu.vn>
2023-09-28 00:47:50 +0700

    add request

--- src/main/java/team2/elearningapplication/dto/request/DeleteCategory.java
+++ src/main/java/team2/elearningapplication/dto/request/DeleteCategory.java
@@ -3,8 +3,12 @@ package team2.elearningapplication.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Setter
 @Getter
 public class DeleteCategory {
-
+    @NotBlank
+    private String name;
+    
 }
