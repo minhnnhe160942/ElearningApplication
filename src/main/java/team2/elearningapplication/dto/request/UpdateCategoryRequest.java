25442bfe026819644e862aa572523a9c691d0ce0
minhnnhe160942 <minhnnhe160942@fpt.edu.vn>
2023-10-15 00:05:46 +0700

    update

--- src/main/java/team2/elearningapplication/dto/request/UpdateCategoryRequest.java
+++
@@ -1,9 +0,0 @@
-package team2.elearningapplication.dto.request;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UpdateCategoryRequest {
-}
--- src/main/java/team2/elearningapplication/service/ICourseService.java
+++
@@ -1,5 +0,0 @@
-package team2.elearningapplication.service;
-
-import team2.elearningapplication.dto.common.ResponseCommon;
-
-}
