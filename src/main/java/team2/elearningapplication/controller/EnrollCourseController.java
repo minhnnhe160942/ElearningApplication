06aaa098d345b7747564689080af104897ec6818
minhnhhe161877 <minhnhhe161877@fpt.edu.vn>
2023-10-16 09:47:22 +0700

    update enroll 13

--- src/main/java/team2/elearningapplication/controller/EnrollCourseController.java
+++ src/main/java/team2/elearningapplication/controller/EnrollCourseController.java
@@ -937,5 +937,311 @@ public QuizResult takeQuiz(@RequestBody List<String> answers) {
         return new QuizResult(score);
         }
         }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+@PostMapping("/quiz")
+public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+        if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+        score++;
+        }
+        }
+        return new QuizResult(score);
+        }
+        }
 
 }
