c6524207321b713eeda7a8ba916c230bc3f135b4
hiepthhe161790 <hiepthhe161790@fpt.edu.vn>
2023-10-16 10:00:30 +0700

    payment controller 3

--- src/main/java/team2/elearningapplication/controller/PaymentController.java
+++ src/main/java/team2/elearningapplication/controller/PaymentController.java
@@ -42,4 +42,14 @@ public class PaymentController {
         return new QuizResult(score);
     }
     payment controller 2
+    @PostMapping("/quiz")
+    public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+            if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+                score++;
+            }
+        }
+        return new QuizResult(score);
+    }
 }
