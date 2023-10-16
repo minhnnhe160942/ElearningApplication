d2cb34820ab216ed79c7c64c32edf40865a0e6b0
hiepthhe161790 <hiepthhe161790@fpt.edu.vn>
2023-10-16 10:03:27 +0700

    payment controller 6 pro

--- src/main/java/team2/elearningapplication/controller/PaymentController.java
+++ src/main/java/team2/elearningapplication/controller/PaymentController.java
@@ -62,5 +62,14 @@ public class PaymentController {
         }
         return new QuizResult(score);
     }
-
+    @PostMapping("/quiz")
+    public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+            if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+                score++;
+            }
+        }
+        return new QuizResult(score);
+    }
 }
