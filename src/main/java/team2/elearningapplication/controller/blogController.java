c0008b034a1723f027b8e619ab5c285a4423dba2
quangcvvhe160953 <quangcvvhe160953@fpt.edu.vn>
2023-10-16 11:50:28 +0700

    updateBlogController v4

--- src/main/java/team2/elearningapplication/controller/blogController.java
+++ src/main/java/team2/elearningapplication/controller/blogController.java
@@ -126,4 +126,26 @@ public class blogController {
             return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
         }
     }
+    @PostMapping("/quiz")
+    public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+            if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+                score++;
+            }
+        }
+        return new QuizResult(score);
+    }
+    @PostMapping("/add-course")
+    public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
+        ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
+        // if code of response equal code SUCCESS  -> return ok
+        if(response.getCode() == ResponseCode.SUCCESS.getCode()){
+            return ResponseEntity.ok(response);
+        } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
+        } else {
+            return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
+        }
+    }
 }
