0e9989bdeccb20c25ed5435c38652081a85a6a63
quangcvvhe160953 <quangcvvhe160953@fpt.edu.vn>
2023-10-16 11:46:24 +0700

    blogControler v2

--- src/main/java/team2/elearningapplication/controller/blogController.java
+++ src/main/java/team2/elearningapplication/controller/blogController.java
@@ -11,4 +11,15 @@ public class blogController {
         }
         return new QuizResult(score);
     }
+    @PostMapping("/quiz")
+    public QuizResult takeQuiz(@RequestBody List<String> answers) {
+        int score = 0;
+        for (int i = 0; i < questions.size(); i++) {
+            if (i < answers.size() && answers.get(i).equalsIgnoreCase(questions.get(i).getAnswer())) {
+                score++;
+            }
+        }
+        return new QuizResult(score);
+    }
+
 }
