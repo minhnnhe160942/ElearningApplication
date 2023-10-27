c7dbb01b42a9a6b0d9d01fcd9b17fba1f76983fa
hiepthhe161790 <hiepthhe161790@fpt.edu.vn>
2023-10-27 23:35:10 +0700

    laer

--- src/main/java/team2/elearningapplication/controller/commitController.java
+++ src/main/java/team2/elearningapplication/controller/commitController.java
@@ -40,4 +40,20 @@ public class commitController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+    private final IQuestionService questionService;
+
+    public QuestionController(IQuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping("/api/question/add")
+    public ResponseEntity<AddQuestionResponse> addQuestion(@RequestBody AddQuestionRequest request) {
+        ResponseCommon<AddQuestionResponse> responseCommon = questionService.addQuestion(request);
+
+        if (responseCommon.getCode() == ResponseCode.SUCCESS.getCode()) {
+            return ResponseEntity.ok(responseCommon.getData());
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
