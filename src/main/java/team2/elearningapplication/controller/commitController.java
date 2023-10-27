8dce969fd007e26d0e15dd3fc492271bffa468c9
hiepthhe161790 <hiepthhe161790@fpt.edu.vn>
2023-10-27 23:32:39 +0700

    hhe

--- src/main/java/team2/elearningapplication/controller/commitController.java
+++ src/main/java/team2/elearningapplication/controller/commitController.java
@@ -1,4 +1,27 @@
 package team2.elearningapplication.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/lesson")
+@AllArgsConstructor
 public class commitController {
+    private final IQuestionService questionService;
+
+    public QuestionController(IQuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping("/api/question/add")
+    public ResponseEntity<AddQuestionResponse> addQuestion(@RequestBody AddQuestionRequest request) {
+        ResponseCommon<AddQuestionResponse> responseCommon = questionService.addQuestion(request);
+
+        if (responseCommon.getCode() == ResponseCode.SUCCESS.getCode()) {
+            return ResponseEntity.ok(responseCommon.getData());
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
