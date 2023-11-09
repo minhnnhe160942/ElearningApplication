6c5f602deabd54b01949848214300daf631122bb
hiepthhe161790 <hiepthhe161790@fpt.edu.vn>
2023-11-10 03:17:49 +0700

    2

--- src/main/java/team2/elearningapplication/controller/DaihoiCommitController.java
+++ src/main/java/team2/elearningapplication/controller/DaihoiCommitController.java
@@ -22,4 +22,15 @@ public class DaihoiCommitController {
             return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(), "Add post failed", null));
         }
     }
+    @PostMapping("/add-post")
+    public ResponseEntity<ResponseCommon<AddPostResponse>> addPost(@Valid @RequestBody AddPostRequest addPostRequest) {
+        ResponseCommon<AddPostResponse> response = postService.addPost(addPostRequest);
+        if (response.getCode() == ResponseCode.SUCCESS.getCode()) {
+            return ResponseEntity.ok(response);
+        } else if (response.getCode() == ResponseCode.POST_CONTENT_IS_EMPTY.getCode()) {
+            return ResponseEntity.badRequest().body(new ResponseCommon<>(response.getCode(), "Post content is empty", null));
+        } else {
+            return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(), "Add post failed", null));
+        }
+    }
 }
