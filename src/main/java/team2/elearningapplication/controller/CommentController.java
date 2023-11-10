d4e0ff67a93fbaf75b1dab75d8ea694584941fc5
quangcvvhe160953 <quangcvvhe160953@fpt.edu.vn>
2023-11-11 00:52:19 +0700

    fix comment

--- src/main/java/team2/elearningapplication/controller/CommentController.java
+++ src/main/java/team2/elearningapplication/controller/CommentController.java
@@ -6,78 +6,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import team2.elearningapplication.Enum.ResponseCode;
 import team2.elearningapplication.dto.common.ResponseCommon;
-import team2.elearningapplication.dto.request.admin.course.AddCourseRequest;
-import team2.elearningapplication.dto.response.admin.course.AddCourseResponse;
-import team2.elearningapplication.service.ICourseService;
-import team2.elearningapplication.service.IPaymentService;
+import team2.elearningapplication.dto.request.admin.category.AddCategoryRequest;
+import team2.elearningapplication.dto.response.admin.category.AddCategoryResponse;
+import team2.elearningapplication.service.ICategoryService;
 
 import javax.validation.Valid;
 
 public class CommentController {
-    private ICourseService courseService;
-    private IPaymentService paymentService;
-    private final int TOP_COURSE = 10;
-    @PostMapping("/add-course")
-    public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
-        ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
-        // if code of response equal code SUCCESS  -> return ok
+    private ICategoryService categoryService;
+    @PostMapping("/add-category")
+    public ResponseEntity<ResponseCommon<AddCategoryResponse>> addCategory(@Valid @RequestBody AddCategoryRequest addCategoryRequest){
+        ResponseCommon<AddCategoryResponse> response = categoryService.addCategory(addCategoryRequest);
+        // if code response equals code Success -> return ok
         if(response.getCode() == ResponseCode.SUCCESS.getCode()){
             return ResponseEntity.ok(response);
-        } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
-        } else {
-            return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
-        }   public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
-            ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
-            // if code of response equal code SUCCESS  -> return ok
-            if(response.getCode() == ResponseCode.SUCCESS.getCode()){
-                return ResponseEntity.ok(response);
-            } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
-            } else {
-                return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
-            }   public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
-                ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
-                // if code of response equal code SUCCESS  -> return ok
-                if(response.getCode() == ResponseCode.SUCCESS.getCode()){
-                    return ResponseEntity.ok(response);
-                } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
-                } else {
-                    return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
-                }
-    }public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
-                ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
-                // if code of response equal code SUCCESS  -> return ok
-                if(response.getCode() == ResponseCode.SUCCESS.getCode()){
-                    return ResponseEntity.ok(response);
-                } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
-                } else {
-                    return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
-                }
-            }public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
-                ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
-                // if code of response equal code SUCCESS  -> return ok
-                if(response.getCode() == ResponseCode.SUCCESS.getCode()){
-                    return ResponseEntity.ok(response);
-                } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
-                } else {
-                    return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
-                }
-            }
-            public ResponseEntity<ResponseCommon<AddCourseResponse>> addCourse(@Valid @RequestBody AddCourseRequest addCourseRequest){
-                ResponseCommon<AddCourseResponse> response = courseService.addCourse(addCourseRequest);
-                // if code of response equal code SUCCESS  -> return ok
-                if(response.getCode() == ResponseCode.SUCCESS.getCode()){
-                    return ResponseEntity.ok(response);
-                } else if(response.getCode() == ResponseCode.COURSE_EXIST.getCode()) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(response.getCode(),"Course already exsit",null));
-                } else {
-                    return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"Add course fail",null));
-                }
-            }
-            el
-
+        } // if code response equals code category exist -> return error
+        else if(response.getCode() == ResponseCode.CATEGORY_EXIST.getCode()){
+            return  ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(ResponseCode.CATEGORY_EXIST.getCode(),"Category already exist",null));
+        } // else return fail
+        else {
+            return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"System error",null));
+        }
+    }@PostMapping("/add-category")
+    public ResponseEntity<ResponseCommon<AddCategoryResponse>> addCategory(@Valid @RequestBody AddCategoryRequest addCategoryRequest){
+        ResponseCommon<AddCategoryResponse> response = categoryService.addCategory(addCategoryRequest);
+        // if code response equals code Success -> return ok
+        if(response.getCode() == ResponseCode.SUCCESS.getCode()){
+            return ResponseEntity.ok(response);
+        } // if code response equals code category exist -> return error
+        else if(response.getCode() == ResponseCode.CATEGORY_EXIST.getCode()){
+            return  ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseCommon<>(ResponseCode.CATEGORY_EXIST.getCode(),"Category already exist",null));
+        } // else return fail
+        else {
+            return ResponseEntity.badRequest().body(new ResponseCommon<>(ResponseCode.FAIL.getCode(),"System error",null));
+        }
+    }
 }
